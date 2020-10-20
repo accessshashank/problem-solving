58da4dd07d25e79cc4d5e2c989cef2ebe8b12a54
Shashank Shekhar <shekharshashank@outlook.com>
2020-10-21 00:25:05 +0530

    more-ll-programs

---
+++ Practice/LinkedList/ConcatenateLL.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.LinkedList
+{
+    class ConcatenateLL
+    {
+        static void Main(string[] args)
+        {
+            var numArray = new int[] { 5, 10, 13, 12, 18, 20, 25 };
+            var ll1 = LLHelper.CreateLinkedList(numArray);
+            Console.WriteLine("First LL");
+            LLHelper.Display(ll1.Head);
+            Console.WriteLine();
+
+            var numArray1 = new int[] { 100, 130, 150 };
+            var ll2 = LLHelper.CreateLinkedList(numArray1);
+            Console.WriteLine("Second LL");
+            LLHelper.Display(ll2.Head);
+            Console.WriteLine();
+
+            var llConcat = Concatenate(ll1, ll2);
+            Console.WriteLine("Cocatenated LL");
+            LLHelper.Display(llConcat.Head);
+            Console.WriteLine();
+
+        }
+
+        private static LinkedList Concatenate(LinkedList ll1, LinkedList ll2)
+        {
+            var temp = ll1.Head;
+
+            while(temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = ll2.Head;
+
+            return ll1;
+        }
+    }
+}
--- Practice/LinkedList/ReverseLL.cs
+++ Practice/LinkedList/ReverseLL.cs
@@ -10,15 +10,74 @@ namespace Practice.LinkedList
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1. Reversing LL using auxiliary array");
             var numArray = new int[] { 5, 10, 13, 12, 18, 20, 25 };
             var ll = LLHelper.CreateLinkedList(numArray);
             Console.WriteLine("Input LL");
             LLHelper.Display(ll.Head);
             Console.WriteLine();
-            Console.WriteLine("Reversing LL using auxiliary array");
             ll = ReverseUsingArray(ll);
             LLHelper.Display(ll.Head);
             Console.WriteLine();
+
+            Console.WriteLine("2. Reversing LL using sliding pointers");
+            ll = LLHelper.CreateLinkedList(numArray);
+            Console.WriteLine("Input LL");
+            LLHelper.Display(ll.Head);
+            Console.WriteLine();
+            ll = ReverseUsingSlidingPointers(ll);
+            LLHelper.Display(ll.Head);
+            Console.WriteLine();
+
+            Console.WriteLine("3. Reversing LL using recursion");
+            ll = LLHelper.CreateLinkedList(numArray);
+            Console.WriteLine("Input LL");
+            LLHelper.Display(ll.Head);
+            Console.WriteLine();
+            ll = ReverseUsingRecursion(ll);
+            LLHelper.Display(ll.Head);
+            Console.WriteLine();
+        }
+
+        private static LinkedList ReverseUsingRecursion(LinkedList ll)
+        {
+            Node previous = null;
+            Node current = ll.Head;
+
+            ReverseRecursively(previous, current, ll);
+
+            return ll;
+        }
+
+        private static void ReverseRecursively(Node previous, Node current, LinkedList ll)
+        {
+            if(current != null)
+            {
+                ReverseRecursively(current, current.Next, ll);
+                current.Next = previous;
+            }
+            else
+            {
+                ll.Head = previous;
+            }
+        }
+
+        private static LinkedList ReverseUsingSlidingPointers(LinkedList ll)
+        {
+            Node r = null;
+            Node q = null;
+            Node p = ll.Head;
+
+            while(p != null)
+            {
+                r = q;
+                q = p;
+                p = p.Next;
+                q.Next = r; 
+            }
+
+            ll.Head = q;
+            return ll;
         }
 
         private static LinkedList ReverseUsingArray(LinkedList ll)
