e1ce5aeb9b15e0f9484f3cd1fddd3e5fcef2b55d
Shashank Shekhar <shekharshashank@outlook.com>
2020-10-23 00:49:37 +0530

    stack-impl

---
+++ Practice/Stack/StackUsingArray.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Stack
+{
+    class StackUsingArray
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter size of stack : ");
+            Console.ReadLine();
+            Console.WriteLine("Accepting size 3 : ");
+            int size = 3;
+
+            var stack = new Stack(size);
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("Is stack full : "+ stack.IsFull());
+            Console.WriteLine("Peek element :{0}", stack.Peek());
+            Console.WriteLine("Pop element :{0}", stack.Pop());
+            Console.WriteLine("Pop element :{0}", stack.Pop());
+            Console.WriteLine("Pop element :{0}", stack.Pop());
+            Console.WriteLine("Is stack empty : " + stack.IsEmpty());
+        }
+    }
+
+    public class Stack
+    {
+        public int Size { get; private set; }
+        private int Top { get; set; }
+        private readonly int[] arr;
+
+        public Stack(int size)
+        {
+            Size = size;
+            Top = -1;
+            arr = new int[size];
+        }
+
+        public void Push(int val)
+        {
+            if (IsFull()) return;
+
+            arr[++Top] = val;
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty()) return -1;
+
+            return arr[Top--];
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty()) return -1;
+            return arr[Top];
+        }
+
+        public bool IsEmpty()
+        {
+            return Top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return Top == Size - 1;
+        }
+    }
+}
---
+++ Practice/Stack/StackUsingLL.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Local = Practice.Stack.UsingLinkedList;
+
+namespace Practice.Stack
+{
+    class StackUsingLL
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack using linked list");
+            var stack = new Local.Stack();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("Peek element :{0}", stack.Peek());
+            Console.WriteLine("Pop element :{0}", stack.Pop());
+            Console.WriteLine("Pop element :{0}", stack.Pop());
+            Console.WriteLine("Pop element :{0}", stack.Pop());
+            Console.WriteLine("Is stack empty : " + stack.IsEmpty());
+        }
+    }
+
+}
+
+namespace Practice.Stack.UsingLinkedList
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+
+        public void Push(int val)
+        {
+            if(Top == null)
+            {
+                Top = new Node();
+                Top.Value = val;
+                Top.Next = null;
+            }
+            else
+            {
+                var node = new Node();
+                node.Value = val;
+                node.Next = Top;
+                Top = node;
+            }
+        }
+
+        public int Pop()
+        {
+            if (IsEmpty()) return -1;
+
+            var current = Top;
+            var value = Top.Value;
+            Top = Top.Next;
+            current.Next = null;
+
+            return value;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty()) return -1;
+            return Top.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return Top == null;
+        }
+
+        public class Node
+        {
+            public int Value { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+
+
+
+}
