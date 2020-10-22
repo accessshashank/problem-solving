3b57c6c2cf96c7c50446dd6c8747671e6c3d08cc
Shashank Shekhar <shekharshashank@outlook.com>
2020-10-22 08:57:06 +0530

    more-ll-programs

---
+++ Practice/LinkedList/CircularDoublyLL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.LinkedList
+{
+    class CircularDoublyLL
+    {
+        static void Main(string[] args)
+        {
+            var numArray = new int[] { 1, 2, 3, 4, 5 };
+            var dll = LLHelper.CreateDoublyLinkedList(numArray);
+            var termFirst = dll.Head;
+            var termLast = dll.Tail;
+            termFirst.Previous = termLast;
+            termLast.Next = termFirst;
+
+            Display(dll);
+
+            Console.WriteLine("Inserting node 100 at first position");
+            dll = InsertNodeAtFirstPosition(dll);
+
+            Display(dll);
+        }
+
+        private static DoublyLinkedList InsertNodeAtFirstPosition(DoublyLinkedList dll)
+        {
+            var node = new DoublyNode();
+            node.Value = 100;
+            var head = dll.Head;
+            var pointerToLastNode = head.Previous;
+            
+            node.Next = head;
+            head.Previous = node;
+            node.Previous = pointerToLastNode;
+            pointerToLastNode.Next = node;
+
+            dll.Head = node;
+            return dll;
+        }
+
+        private static void Display(DoublyLinkedList dll)
+        {
+            Console.WriteLine("Display of circular doubly linked list");
+            var p = dll.Head;
+
+            do 
+            {
+                Console.Write(p.Value + "->");
+                p = p.Next;
+            } while (p != dll.Head);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Practice/LinkedList/ReverseDoublyLL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.LinkedList
+{
+    class ReverseDoublyLL
+    {
+        static void Main(string[] args)
+        {
+            var numArray = new int[] { 1,2,3,4,5 };
+            var dll = LLHelper.CreateDoublyLinkedList(numArray);
+
+            Console.WriteLine("Display Forward");
+            LLHelper.DisplayDoublyForward(dll.Head);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Display Backward");
+            LLHelper.DisplayDoublyBackward(dll.Tail);
+
+            Console.WriteLine();
+            Console.WriteLine("Reversing Doubly LL");
+            dll = Reverse(dll);
+
+            Console.WriteLine("Display Forward");
+            LLHelper.DisplayDoublyForward(dll.Head);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Display Backward");
+            LLHelper.DisplayDoublyBackward(dll.Tail);
+
+            Console.WriteLine();
+        }
+
+        private static DoublyLinkedList Reverse(DoublyLinkedList dll)
+        {
+            var p = dll.Head;   
+            dll.Tail = p;
+            while(p != null)
+            {
+                var temp = p.Next;
+                p.Next = p.Previous;
+                p.Previous = temp;
+                if(p.Previous == null)
+                {
+                    dll.Head = p;
+                }
+                p = p.Previous;
+            }
+
+            return dll;
+        }
+    }
+}
