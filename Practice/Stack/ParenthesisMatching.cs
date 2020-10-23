9f516436839ddbf98775729a2e5198089eb65576
Shashank Shekhar <shekharshashank@outlook.com>
2020-10-23 09:22:30 +0530

    more-stack

---
+++ Practice/Stack/ParenthesisMatching.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Stack
+{
+    class ParenthesisMatching
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Parenthesis : ");
+            var str = Console.ReadLine();
+            bool isBalanced = CheckBalance(str);
+            if (isBalanced) Console.WriteLine("Parenthesis IS balanced");
+            else Console.WriteLine("Parenthesis is NOT balanced");
+        }
+
+        private static bool CheckBalance(string str)
+        {
+            var stack = new GenericStack<char>(str.Length);
+
+            for(int i=0; i< str.Length; i++)
+            {
+                if (str[i] == '(' || str[i] == '[' || str[i] == '{') 
+                    stack.Push(str[i]);
+                else if(str[i] == ')' || str[i] == ']' || str[i] == '}')
+                {
+                    if (stack.IsEmpty()) return false;
+                    char bracket = stack.Pop();
+                    if (str[i] == ')' && bracket != '(') return false;
+                    if (str[i] == ']' && bracket != '[') return false;
+                    if (str[i] == '}' && bracket != '{') return false;
+                }
+            }
+
+            if (stack.IsEmpty()) return true;
+            return false;
+        }
+    }
+}
---
+++ Practice/Stack/StackUsingGeneric.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Stack
+{
+    class StackUsingGeneric
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Interger Stack");
+            Console.WriteLine("Accepting size 3 : ");
+            int size = 3;
+            var intStack = new GenericStack<int>(size);
+
+            intStack.Push(1);
+            intStack.Push(2);
+            intStack.Push(3);
+
+            Console.WriteLine("Is stack full : " + intStack.IsFull());
+            Console.WriteLine("Peek element :{0}", intStack.Peek());
+            Console.WriteLine("Pop element :{0}", intStack.Pop());
+            Console.WriteLine("Pop element :{0}", intStack.Pop());
+            Console.WriteLine("Pop element :{0}", intStack.Pop());
+            Console.WriteLine("Is stack empty : " + intStack.IsEmpty());
+
+            Console.WriteLine("Initializing Character Stack");
+            Console.WriteLine("Accepting size 3 : ");
+            var charStack = new GenericStack<char>(size);
+
+            charStack.Push('A');
+            charStack.Push('B');
+            charStack.Push('C');
+
+            Console.WriteLine("Is stack full : " + charStack.IsFull());
+            Console.WriteLine("Peek element :{0}", charStack.Peek());
+            Console.WriteLine("Pop element :{0}", charStack.Pop());
+            Console.WriteLine("Pop element :{0}", charStack.Pop());
+            Console.WriteLine("Pop element :{0}", charStack.Pop());
+            Console.WriteLine("Is stack empty : " + charStack.IsEmpty());
+
+        }
+    }
+
+    public class GenericStack<T>
+    {
+        public int Size { get; private set; }
+        private int Top { get; set; }
+        private readonly T[] arr;
+
+        public GenericStack(int size)
+        {
+            Size = size;
+            Top = -1;
+            arr = new T[size];
+        }
+
+        public void Push(T val)
+        {
+            if (IsFull()) return;
+
+            arr[++Top] = val;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty()) return default(T);
+
+            return arr[Top--];
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty()) return default(T);
+            return arr[Top];
+        }
+
+        public bool IsEmpty()
+        {
+            return Top == -1;
+        }
+
+        public bool IsFull()
+        {
+            return Top == Size - 1;
+        }
+    }
+}
