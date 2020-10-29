8cb0a4667e73e5c3cb31a365bc4827b5e6dae654
Shashank Shekhar <shekharshashank@outlook.com>
2020-10-29 21:53:52 +0530

    more-bst-programs

---
+++ Practice/BinarySearchTree/DeleteInBinarySearchTree.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.BinarySearchTree
+{
+    class DeleteInBinarySearchTree
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating binary search tree ... !");
+            int[] input = new int[] { 9, 15, 5, 20, 16, 8, 12, 3, 6 };
+            Console.Write("Input Array - ({0})", string.Join(",", input));
+            var root = BinarySearchTreeHelper.InitializeBinarySearchTree(input);
+            Console.WriteLine();
+            Console.WriteLine("Inorder traversal ... !");
+            InOrder(root);
+            Console.WriteLine();
+            Console.WriteLine("Deleting node 8");
+            Delete(root, root, 8);
+            Console.WriteLine("Inorder traversal ... !");
+            InOrder(root);
+            Console.WriteLine();
+            Console.WriteLine("Deleting node 9");
+            Delete(root, root, 9);
+            Console.WriteLine("Inorder traversal ... !");
+            InOrder(root);
+            Console.WriteLine();
+        }
+
+        private static TreeNode Delete(TreeNode root, TreeNode p, int key)
+        {
+            TreeNode q = null;
+
+            if (p == null) return null;
+
+            if(p.Left == null && p.Right == null)
+            {
+                if(p == root)
+                {
+                    root = null;
+                }
+                return null;
+            }
+
+            if(key > p.Value)
+            {
+                p.Right = Delete(root, p.Right, key);
+            }
+            else if (key < p.Value)
+            {
+                p.Left = Delete(root, p.Left, key);
+            }
+            else
+            {
+                if(Height(p.Left) > Height(p.Right))
+                {
+                    q = FindInorderPredecessor(p.Left);
+                    p.Value = q.Value;
+                    p.Left = Delete(root, p.Left, q.Value);
+                }
+                else
+                {
+                    q = FindInorderSuccessor(p.Right);
+                    p.Value = q.Value;
+                    p.Right = Delete(root, p.Right, q.Value);
+                }
+            }
+
+
+            return p;
+        }
+
+        // InorderSuccessor is left most node of right subtree. See the input passed from calling function is right node (of p)
+        private static TreeNode FindInorderSuccessor(TreeNode p)
+        {
+            while(p != null && p.Left != null)
+            {
+                p = p.Left;
+            }
+            return p;
+        }
+
+        //InorderPredecessor is the right most node of left subtree. See the input passed from calling function is left node (of p)
+        private static TreeNode FindInorderPredecessor(TreeNode p)
+        {
+            while(p != null && p.Right != null)
+            {
+                p = p.Right;
+            }
+            return p;
+        }
+
+        private static int Height(TreeNode p)
+        {
+            if (p == null) return 0;
+            int x = Height(p.Left);
+            int y = Height(p.Right);
+            if(x > y)
+            {
+                return x + 1;
+            }
+            return y + 1;
+        }
+
+        private static void InOrder(TreeNode node)
+        {
+            if (node != null)
+            {
+                InOrder(node.Left);
+                Console.Write("({0}), ", node.Value);
+                InOrder(node.Right);
+            }
+        }
+    }
+}
---
+++ Practice/BinarySearchTree/GenerateBSTUsingPreorder.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practice.Stack;
+
+namespace Practice.BinarySearchTree
+{
+    class GenerateBSTUsingPreorder
+    {
+        static void Main(string[] args)
+        {
+            int[] inputPreorder = new int[] { 30, 20, 10, 15, 25, 40, 50, 45 };
+            Console.Write("Input Preorder Array - ({0})", string.Join(",", inputPreorder));
+            Console.WriteLine();
+            Console.WriteLine("Generating BST using Preorder array");
+            TreeNode root1 = GenerateTreeFromPreorder(inputPreorder);
+            Console.WriteLine("Finished generating BST using Preorder array");
+            Console.WriteLine("Preorder traversal for dual verification ... !");
+            PreOrder(root1);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int[] inputPostorder = new int[] { 15, 10, 25, 20, 45, 50, 40, 30 };
+            Console.Write("Input Postorder Array - ({0})", string.Join(",", inputPostorder));
+            Console.WriteLine();
+            Console.WriteLine("Generating BST using Postorder array");
+            TreeNode root2 = GenerateTreeFromPostorder(inputPostorder);
+            Console.WriteLine("Finished generating BST using Postorder array");
+            Console.WriteLine("Postorder traversal for dual verification ... !");
+            PostOrder(root2);
+            Console.WriteLine();
+
+        }
+
+        private static TreeNode GenerateTreeFromPostorder(int[] input)
+        {
+            TreeNode temp = null;
+            TreeNode p = null;
+            int i = input.Length - 1;
+            TreeNode root = new TreeNode();
+            root.Value = input[i--];
+            if (input.Length == 1) return root;
+
+            var stack = new GenericStack<TreeNode>(input.Length);
+            p = root;
+            while (i >= 0)
+            {
+                if (input[i] < p.Value)
+                {
+                    temp = new TreeNode();
+                    temp.Value = input[i--];
+                    p.Left = temp;
+                    stack.Push(p);
+                    p = temp;
+                }
+                else
+                {
+                    if (input[i] > p.Value && (stack.IsEmpty() || input[i] < stack.Peek().Value))
+                    {
+                        temp = new TreeNode();
+                        temp.Value = input[i--];
+                        p.Right = temp;
+                        p = temp;
+                    }
+                    else
+                    {
+                        p = stack.Pop();
+                    }
+                }
+            }
+
+            return root;
+        }
+
+        private static void PreOrder(TreeNode node)
+        {
+            if (node != null)
+            {
+                Console.Write("{0}, ", node.Value);
+                PreOrder(node.Left);
+                PreOrder(node.Right);
+            }
+        }
+
+        private static void PostOrder(TreeNode node)
+        {
+            if (node != null)
+            {
+                PostOrder(node.Left);
+                PostOrder(node.Right);
+                Console.Write("({0}), ", node.Value);
+            }
+        }
+
+        private static TreeNode GenerateTreeFromPreorder(int[] input)
+        {
+            TreeNode temp = null;
+            TreeNode p = null;
+            int i = 0;
+            TreeNode root = new TreeNode();
+            root.Value = input[i++];
+            if (input.Length == 1) return root;
+
+            var stack = new GenericStack<TreeNode>(input.Length);
+            p = root;
+            while(i < input.Length)
+            {
+                if(input[i] < p.Value)
+                {
+                    temp = new TreeNode();
+                    temp.Value = input[i++];
+                    p.Left = temp;
+                    stack.Push(p);
+                    p = temp;
+                }
+                else
+                {
+                    if(input[i] > p.Value && (stack.IsEmpty() || input[i] < stack.Peek().Value))
+                    {
+                        temp = new TreeNode();
+                        temp.Value = input[i++];
+                        p.Right = temp;
+                        p = temp;
+                    }
+                    else
+                    {
+                        p = stack.Pop();
+                    }
+                }
+            }
+
+            return root;
+        }
+    }
+}
