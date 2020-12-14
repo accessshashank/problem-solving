34a980731f96fdf1ea09cf0bb030ca6d8b39018c
Shashank Shekhar <shekharshashank@outlook.com>
2020-12-15 00:02:28 +0530

    sorting-algos

---
+++ Practice/Sort/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Sort
+{
+    class BubbleSort
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 8, 5, 7, 3, 2 };
+            Console.WriteLine("input array ({0})", string.Join(",", input));
+
+            var output = BubbleSortFunc(input, input.Length);
+            Console.WriteLine("output array ({0})", string.Join(",", output));
+        }
+
+        private static int[] BubbleSortFunc(int[] input, int length)
+        {
+            for(int i=0; i< length-1; i++)
+            {
+                for (int j = 0; j < length - 1 - i; j++)
+                {
+                    if(input[j] > input[j+1])
+                    {
+                        var temp = input[j + 1];
+                        input[j + 1] = input[j];
+                        input[j] = temp;
+                    }
+                }
+            }
+
+            return input;
+        }
+    }
+}
---
+++ Practice/Sort/InsertionSort.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Sort
+{
+    class InsertionSort
+    {
+        static void Main(string[] args)
+        {
+            var input = new int[] { 8, 5, 7, 3, 2 };
+            Console.WriteLine("input array ({0})", string.Join(",", input));
+
+            var output = InsertionSortFunc(input, input.Length);
+            Console.WriteLine("output array ({0})", string.Join(",", output));
+        }
+
+        private static int[] InsertionSortFunc(int[] input, int length)
+        {
+            for(int i = 1; i<length; i++)
+            {
+                int j = i - 1;
+                var x = input[i];
+
+                while(j> -1 && input[j] > x)
+                {
+                    input[j + 1] = input[j];
+                    j--;
+                }
+
+                input[j+1] = x;
+            }
+
+            return input;
+        }
+    }
+}
