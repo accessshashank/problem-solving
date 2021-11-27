d0f1cb6fe3a57ecaca1b7d4313d733b548f62092
Shashank Shekhar <shekharshashank@outlook.com>
2021-11-27 23:00:28 +0530

    recursion

---
+++ Practice/Recursion/CheckArraySorted.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Recursion
+{
+    class CheckArraySorted
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, 2, 5 ,8, 9, 12, 43, 40 };
+            Console.WriteLine(Sorted(array));
+        }
+
+        private static bool Sorted(int[] array)
+        {
+            return helper(array, 0, array.Length-1);
+        }
+
+        private static bool helper(int[] array, int start, int length)
+        {
+            if (start == length) return true;
+
+            return array[start] < array[start + 1] && helper(array, start + 1, length);
+        }
+    }
+}
---
+++ Practice/Recursion/Triangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Recursion
+{
+    class Triangle
+    {
+        static void Main(string[] args)
+        {
+             
+            TrianglePrint(4,1);
+            Console.WriteLine();
+        }
+
+        private static void TrianglePrint(int row, int col)
+        {
+            if (row == 0) return;
+
+            if(col<=row)
+            {
+                //Console.Write("*");
+                TrianglePrint(row, col + 1);
+                Console.Write("*");
+            }
+            else
+            {
+                //Console.WriteLine();
+                TrianglePrint(row - 1, 1);
+                Console.WriteLine();
+            }
+        }
+    }
+}
