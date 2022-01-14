084bce286c1b0fa109e7f3f7b60aace6a3f4b5a7
Shashank Shekhar <shekharshashank@outlook.com>
2022-01-14 22:53:34 +0530

    problems

---
+++ Practice/Recursion/UniqueSubset.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Recursion
+{
+    class UniqueSubset
+    {
+        static void Main(string[] args)
+        {
+            //https://www.udemy.com/course/cpp-data-structures-algorithms-prateek-narang/learn/quiz/5282968#overview
+            int[] arr = new int[] { 1, 2, 2};
+            var result = SubsetsWithDup(arr) ;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(string.Join(",", item));
+            }
+        }
+
+        private static IList<IList<int>> SubsetsWithDup(int[] nums)
+        {
+            Array.Sort(nums);
+            var result = new List<IList<int>>();
+            Subsets(nums, 0, new List<int>(), result);
+            return result; 
+        }
+
+        private static void Subsets(int[] nums, int startIndex, IList<int> oneResult, IList<IList<int>> result)
+        {
+            result.Add(new List<int>(oneResult));
+
+            var n = nums.Length;
+
+            for (int i = startIndex; i < n; i++)
+            {
+                if (i > startIndex && nums[i - 1] == nums[i]) continue;
+
+                oneResult.Add(nums[i]);
+                Subsets(nums, i + 1, oneResult, result);
+                oneResult.RemoveAt(oneResult.Count - 1);
+            }
+        }
+    }
+}
---
+++ Practice/Recursion/WordSearch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Recursion
+{
+    class WordSearch
+    {
+        public static bool chk = false;
+        static void Main(string[] args)
+        {
+            int rows = 3;
+            int cols = 4;
+            char[,] board = new char[,] {
+            {'A','B','C','E' },
+            {'S','F','C','S' },
+            {'A','D','E','E' } };
+
+            string word = "ABCCED";
+            Console.WriteLine(Process(board, rows, cols, word));
+        }
+
+        private static bool Process(char[,] board, int rows, int cols, string word)
+        {
+            chk = false;
+            for(int i = 0; i<rows;i++)
+            {
+                for(int j=0; j<cols; j++)
+                {
+                    if(board[i,j] == word[0])
+                    {
+                        recur(board, i, j, word, 0);
+                    }
+                }
+            }
+            return chk;
+        }
+
+        private static void recur(char[,] board, int i, int j, string word, int k)
+        {
+            if(k == word.Length)
+            {
+                chk = true;
+                return;
+            }
+
+            if(i < 0 || j < 0 || i > 2 || j > 3 || board[i,j] == '@')
+            {
+                return;
+            }
+
+            if(board[i,j] == word[k])
+            {
+                char ch = board[i, j];
+                board[i, j] = '@';
+                recur(board, i, j+1, word, k + 1);
+                recur(board, i + 1, j, word, k + 1);
+                recur(board, i, j - 1, word, k + 1);
+                recur(board, i - 1, j, word, k + 1);
+                board[i, j] = ch;
+            }
+        }
+    }
+}
