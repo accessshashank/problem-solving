4e159af2e8c0e96fc31b09c3de47311fd9e52c7d
Shashank Shekhar <shekharshashank@outlook.com>
2022-02-01 07:57:15 +0530

    d&c

---
+++ Practice/DivideAndConquer/LongestCommonSubsequence.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.DivideAndConquer
+{
+    class LongestCommonSubsequence
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "elephant";
+            string s2 = "eretpat";
+
+            Console.WriteLine(LCS(s1, s2, 0, 0));
+
+            s1 = "houdini";
+            s2 = "hdupti";
+
+            Console.WriteLine(LCS(s1, s2, 0, 0));
+        }
+
+        private static int LCS(string s1, string s2, int i1, int i2)
+        {
+            if (i1 == s1.Length || i2 == s2.Length)
+            {
+                return 0;
+            }
+
+            int c1 = 0;
+            if(s1[i1] == s2[i2])
+            {
+                c1 = 1 + LCS(s1, s2, i1 + 1, i2 + 1);
+            }
+
+            int c2 = LCS(s1, s2, i1 + 1, i2);
+            int c3 = LCS(s1, s2, i1, i2 + 1);
+
+            return Math.Max(c1, Math.Max(c2, c3));
+        }
+    }
+}
---
+++ Practice/DivideAndConquer/LongestPalindromicSubsequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.DivideAndConquer
+{
+    class LongestPalindromicSubsequence
+    {
+        static void Main(string[] args)
+        {
+            string s = "ELRMENMET"; //EMEME is palindrome
+            int startIndex = 0;
+            int endIndex = s.Length - 1;
+
+            Console.WriteLine(LPS(s, startIndex, endIndex));
+        }
+
+        private static int LPS(string str, int startIndex, int endIndex)
+        {
+            if (startIndex > endIndex) return 0;
+            if (startIndex == endIndex) return 1; //on char only so palindrome size will be 1
+
+            int c1 = 0;
+            if(str[startIndex] == str[endIndex])
+            {
+                c1 = 2 + LPS(str, startIndex + 1, endIndex - 1); // 2 because palindrome will be 2 as per current iteration
+            }
+
+            int c2 = LPS(str, startIndex + 1, endIndex);
+            int c3 = LPS(str, startIndex, endIndex - 1);
+
+            return Math.Max(c1, Math.Max(c2, c3));
+        }
+    }
+}
