efd952fb8fd68bae8c1ea66c8e8cfd0ed734c20b
Shashank Shekhar <shekharshashank@outlook.com>
2022-07-19 22:43:53 +0530

    subseq

--- Practice/Recursion/Subsequence.cs
+++ Practice/Recursion/Subsequence.cs
@@ -11,6 +11,8 @@ namespace Practice.Recursion
         static void Main(string[] args)
         {
             Subseq("" , "abc");
+            Console.WriteLine();
+            SubSeq(0, new int[] { 3, 1, 2 }, new List<int>());
         }
 
         private static void Subseq(string processed, string unprocessed)
@@ -25,6 +27,20 @@ namespace Practice.Recursion
 
             Subseq(processed + ch, unprocessed.Substring(1, unprocessed.Length - 1));
             Subseq(processed , unprocessed.Substring(1, unprocessed.Length - 1));
+        }
+
+        private static void SubSeq(int index, int[] arr, List<int> temp)
+        {
+            if(index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(",", temp));
+                return;
+            }
+
+            temp.Add(arr[index]);
+            SubSeq(index + 1, arr, temp);
+            temp.Remove(arr[index]);
+            SubSeq(index + 1, arr, temp);
         }
     }
 }
---
+++ Practice/Recursion/SubsequenceWithGivenSum.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice.Recursion
+{
+    class SubsequenceWithGivenSum
+    {
+        static void Main(string[] args)
+        {
+            int target = 2;
+            int[] arr = new int[] { 1, 2, 1};
+            Sum(0, arr, target, new List<int>());
+        }
+
+        private static void Sum(int index, int[] arr, int target, List<int> temp)
+        {
+            if(target == 0)
+            {
+                Console.WriteLine(string.Join(",", temp));
+                return;
+            }
+            if(index >= arr.Length)
+            {
+                return;
+            }
+
+            if(target >= arr[index])
+            {
+                temp.Add(arr[index]);
+                Sum(index + 1, arr, target - arr[index], temp);
+            }
+
+            temp.Remove(arr[index]);
+            Sum(index + 1, arr, target, temp);
+        }
+    }
+}
